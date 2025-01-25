6901a2fc603943aa96a8e262236384ea827a791d
Alexander Wedekind <awedwork@gmail.com>
2025-01-25 20:24:39 +0000

    adds class and method to return string templates of html elements interpolated with attributes, js script, and nested content

--- server/elementClasses/elementClasses.cs
+++ server/elementClasses/elementClasses.cs
@@ -5,27 +5,158 @@ namespace elements
 {
     public class Elements
     {
-        public string landingPage = "";  
-        public string div = "";
+        public string landingPage = "";
 
-        public string Element(string elementType, string style, string id, string elementClass, string js, string nested)
+        public struct ElementType
         {
-            return $"<{elementType} {style} {id} {elementClass}>{nested}<script>{js}</script></{elementType}>";
+            public ElementType()
+            {
+                string div = "div";
+                string a = "a";
+                string h1 = "h1";
+                string h2 = "h2";
+                string h3 = "h3";
+                string h4 = "h4";
+                string h5 = "h5";
+                string h6 = "h6";
+                string p = "p";
+                string ul = "ul";
+                string ol = "ol";
+                string li = "li";
+                string button = "button";
+                string form = "form"; 
+            }
+            
         }
 
-        public string Div(string style, string id, string elementClass, string js, string nested)
+        public Dictionary<string, string> ElementAttributes = new Dictionary<string, string>();
+        
+        public string Element(string elementType, Dictionary<string, string> attributes, string nested = "", string js = "") //string style = "", string id = "", string elementClass = "")
         {
-            return $"<div {style} {id} {elementClass}>{nested}<script>{js}</script></div>";
+
+        //arguments:
+        
+        //elementType
+        //nested
+        //js
+
+        // global attributes:
+
+        //style
+        //id
+        //class
+        //js
+        //data-* (data passed to element for use by it's js)
+        //title (displays on hover)
+        //lang
+        //tabindex
+        //onclick
+        //onmouseover
+        //hidden
+        //draggable
+        //contenteditable
+        //accesskey
+
+        //element specific attributes:
+
+        //<a>
+            //href
+            //target  (_blank, _self)
+        //button
+            //
+
+            string thisElementType = elementType.ToLower();
+            
+            bool isVoid = false;
+
+            string elementString = "";
+            string openingTag = "";
+            string buildingOpeningTag = $"<{thisElementType}";
+            string closingTag = $"</{thisElementType}>";
+
+
+
+            switch(thisElementType)
+            {
+                case "img":
+                    isVoid = true;
+                    break;
+                case "input":
+                    isVoid = true;
+                    break;
+                case "meta":
+                    isVoid = true;
+                    break;
+                case "link":
+                    isVoid = true;
+                    break;
+                case "hr":
+                    isVoid = true;
+                    break;
+                case "source":
+                    isVoid = true;
+                    break;
+                case "area":
+                    isVoid = true;
+                    break;
+                case "col":
+                    isVoid = true;
+                    break;
+                case "embed":
+                    isVoid = true;
+                    break;
+                case "br":
+                    isVoid = true;
+                    break;
+                default:
+                    break;
+            }
+
+            switch(isVoid)
+            {
+                case true:
+
+                    foreach(var attribute in attributes)
+                    {
+                        if(attribute.Key.ToLower() == thisElementType || attribute.Key.ToLower() == "all")
+                        {
+                            buildingOpeningTag = buildingOpeningTag + " " + $"{attribute.Value}";
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Incorrect html syntax:\nA \" {thisElementType} \" element can't have attribute \" {attribute.Value} \"");
+                        }
+                    }
+
+                    return elementString;
+
+                case false:
+
+                    foreach(var attribute in attributes)
+                    {
+                        if(attribute.Key.ToLower() == thisElementType || attribute.Key.ToLower() == "all")
+                        {
+                            buildingOpeningTag = buildingOpeningTag + $" {attribute.Value}";
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Incorrect html syntax:\nA \" {thisElementType} \" element can't have attribute \" {attribute.Value} \"");
+                        }
+                    }
+
+                    openingTag = buildingOpeningTag + ">";
+                    elementString = openingTag + $"{nested}<script>{js}</script>" + closingTag;
+
+                    return elementString;
+                    ///$"<{thisElementType} {style} {id} {elementClass}>{nested}<script>{js}</script></{thisElementType}>";
+            }
+            
         }
 
-        public string P(string style, string id, string elementClass, string js, string nested)
-        {
-            return $"<p {style} {id} {elementClass}>{nested}<script>{js}</script></p>";
-        }
 
         
 
-        public string LandingPage(string pageTitle, string bodyId, string bodyClass, string js, string nested)
+        public string LandingPage(string pageTitle,string style, string bodyId, string bodyClass, string js, string nested)
             {
                 return
                 $"""
@@ -35,9 +166,9 @@ namespace elements
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>{pageTitle}</title>
-                <link rel="stylesheet" href="landingPage.css">
+                <style>{style}</style>
                 </head>
-                <body {bodyId}{bodyClass}>
+                <body {bodyId} {bodyClass}>
                 {nested}
                 <script>{js}</script>
                 </body>
@@ -46,6 +177,7 @@ namespace elements
             }      
         public Elements()
         {
+            ElementType elementType = new ElementType();
             landingPage = templates.templates.AnyOneFile(MyTimeDiarySharingServer.Listener.absolutePathsPrepend + "/server/templates/htmlTemplates/templateFiles/landingPage.html");
 
             
