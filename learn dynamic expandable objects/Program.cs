b49bec8f42b81107c4f403ee4787f30d1703d70e
Alexander Wedekind <awedwork@gmail.com>
2025-02-27 11:16:07 +0000

    adds practice app to learn dynamic objects

---
+++ learn dynamic expandable objects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Dynamic;
+
+namespace learningDynamicExpandableObjexts;
+
+
+
+class HtmlElement : DynamicObject
+{
+    protected string name;
+    protected string type;
+    protected string openingString;
+    protected string middleString;
+    protected string closingString;
+
+    protected string style;
+
+     
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        HtmlElement Div = new HtmlElement();
+        Div.name = "style";
+    }
+}
