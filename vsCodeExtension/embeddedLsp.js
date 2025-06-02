0c370867848460b37a62d5dc35dbdc84fbc053cb
Alexander Wedekind <awedwork@gmail.com>
2025-06-02 10:14:17 +0100

    changes the document model from array of strings to linked list of Snippet class instances; adds function to refresh the Linked list; Snippet objects have fuctions to return didChange and diOpen objects for sending to server;

--- vsCodeExtension/embeddedLsp.js
+++ vsCodeExtension/embeddedLsp.js
@@ -4,52 +4,232 @@ const rpc = require("vscode-jsonrpc");
 const path = require("path");
 
 let myOutputs;
-let text = "";
-let newText = "";
+let currentText = [];
+let newText = [];
+let documentUri;
 
 
 
 async function getCurrentText() {
-    const currentText = await vscode.window.activeTextEditor.document.getText();
+    const currentText = (await vscode.window.activeTextEditor.document.getText()).split("\n");
     return currentText;
 }
 
-class snippet {
-    constructor(start, text, end){
-        this.start = start;
-        this.text = text;
-        this.end = end;
-    }   
+
+class DocumentSegmentList {
+    constructor(){
+        this.head = null;
+        this.getSegment = (value) => {
+            let currentSegmentNode = this.head;
+            while(currentSegmentNode != null){
+                if(currentSegmentNode.propertyEquals(value) == true){
+                    return currentSegment;
+                }else{
+                    currentSegment = currentSegment.next;
+                }
+            };
+            return null;
+        };
+        this.forEachSegmentNode = (doThis) => {
+            let currentSegment = this.head;
+            while(currentSegment != null){
+                doThis(currentSegment);
+                currentSegment = currentSegment.next;
+            };
+        };
+        this.getLastSegment = () => {
+            let currentSegment = this.head;
+            if(currentSegment != null){
+                while(currentSegment.next != null){
+                    currentSegment = currentSegment.next;
+                }
+            }
+            return currentSegment;
+        }
+
+    }
+    
+};
+
+
+class Snippet {
+    constructor(type, range){
+        if(type == "typescript"){
+            this.type = type;
+            this.range = range;
+            this.uri = (() => {
+                let count = 0;
+                documentSegmentList.forEachSegmentNode(
+                    (segment) => {
+                        if(segment.type == "typescript"){
+                            count ++;
+                        }
+                    }
+                );
+                return `./${count}.ts`;
+            })();
+            this.didOpen = false;
+            this.didChange = false;
+            this.languageId = "typescript";
+            this.version = 1;
+            this.text = (() => {
+                let text = "";
+                let lineIndex = this.range.start.line;
+                do{
+                    if(this.range.start.line == this.range.end.line){
+                        text = `${newText[lineIndex].subString(this.range.start.character, this.range.end.character + 1)}`;
+                    }else if(lineIndex == this.range.start.line){
+                        text = `${text}${newText[lineIndex].subString(this.range.start.character)}`;
+                    }else if(lineIndex == this.range.end.line){
+                        text = `${text}${newText[lineIndex].subString(0, this.range.end.character + 1)}`;
+                    }else if(lineIndex > this.range.start.line && lineIndex < this.range.end.line){
+                        text = `${text}${newText[lineIndex]}`;
+                    }
+                    lineIndex ++;
+                }while(lineIndex != this.range.end.line + 1);
+                return text;
+            })();
+            this.next = null;
+            this.previous = null;
+            this.sendDidOpen = () => {
+                return {
+                    textDocument: {
+                        uri: this.uri,
+                        languageId: this.languageId,
+                        version: this.version,
+                        text: this.text
+                    }
+                };
+            };
+            this.sendDidChange = () => {
+                this.version += 1;
+                return {
+                    textDocument: {
+                        uri: this.uri,
+                        version: this.version
+                    },
+                    contentChanges: [
+                        {
+                            text: this.text
+                        }
+                    ]
+                }
+            };
+            this.propertyEquals = (value) => {
+                let property = [this.type, this.range, this.uri, this.didOpen, this.didChange, this.languageId, this.version, this.text, this.next, this.previous];
+                property.forEach(
+                    (element) => {
+                        if(element == value){
+                            return true;
+                        }
+                    }
+                );
+                return false;
+            }
+        }else if(type == "c-sharp"){
+            this.type = type;
+            this.range = range;
+        }
+    }
 }
 
-let activationCount = 1;
+let documentSegmentList = new DocumentSegmentList();
 
+async function refreshDocumentSegmentList(){
+    newText = await getCurrentText();
+    documentSegmentList = new DocumentSegmentList();
+    let snippetSearchCompleted = false;
+    let boundaryStringIndex = -1;
+    let currentLineNumber = 0;
+    let boundaryStringsFound = 0;
+    let snippetRange = {
+            start: {
+                line: 0,
+                character: 0
+            },
+            end: {
+                line: 0,
+                character: 0
+            }
+        };
+
+    
+    newText.forEach((thisLine) => {
+        do{
+            boundaryStringIndex ++;
+            boundaryStringIndex = thisLine.indexOf("\"\"\"\"", boundaryStringIndex);
+            if(boundaryStringIndex > -1){
+                if(boundaryStringsFound == 0){
+                    snippetRange.end.line = currentLineNumber;
+                    snippetRange.end.character = boundaryStringIndex - 1;
+                    boundaryStringsFound ++;
+                    documentSegmentList.getLastSegment().next = new Snippet("c-sharp", snippetRange);
+                }else if((boundaryStringsFound % 2) == 0){
+                    snippetRange.start.line = snippetRange.end.line;
+                    snippetRange.start.character = snippetRange.end.character + 1;
+                    snippetRange.end.line = currentLineNumber;
+                    snippetRange.end.character = boundaryStringIndex + 3;
+                    documentSegmentList.getLastSegment().next = new Snippet("c-sharp", snippetRange);
+                    snippetRange.start.line = currentLineNumber;
+                    snippetRange.start.character = boundaryStringIndex + 4;
+                    boundaryStringsFound ++;
+                }else if((boundaryStringsFound % 2) == 1){
+                    snippetRange.end.line = currentLineNumber;
+                    snippetRange.end.character = boundaryStringIndex - 1;
+                    documentSegmentList.getLastSegment().next = new Snippet("typescript", snippetRange);
+                    boundaryStringsFound ++;
+                }
+            }else if(boundaryStringIndex == -1 && currentLineNumber == newText.length() - 1 && boundaryStringsFound > 1){
+                snippetRange.start.line = snippetRange.end.line;
+                snippetRange.start.character = snippetRange.end.character + 1;
+                snippetRange.end.line = newText.length() - 1;
+                snippetRange.end.character = newText[newText.length() -1].length() -1;
+                documentSegmentList.getLastSegment().next = new Snippet("c-sharp", snippetRange);
+                boundaryStringsFound ++;
+                snippetSearchCompleted = true;
+            }else{
+                snippetSearchCompleted = true;
+            }
+        }while(snippetSearchCompleted == false);
+        snippetSearchCompleted = false; 
+        currentLineNumber ++;
+    });
+};
+
+const textSnippets = [];
+
+let activationCount = 0;
 
 async function doCompletions() {
-    text = await getCurrentText();
-    let codeSnippets = [];
-    let snippetSearchCompleted = false;
-    let preSnippetBoundaryStringIndex = 0;
-    let postSnippetBoundaryStringIndex = 0;
+    activationCount += 1;
+    myOutputs.appendLine(`\n-- 'DO-COMPLETIONS' ACTIVATED, count: ${activationCount} --`);
     
-    do{
-        preSnippetBoundaryStringIndex = text.indexOf("\"\"\"\"", preSnippetBoundaryStringIndex);
-        postSnippetBoundaryStringIndex = text.indexOf("\"\"\"\"", preSnippetBoundaryStringIndex + 1);
-        if(preSnippetBoundaryStringIndex > -1 && postSnippetBoundaryStringIndex > -1){
-            codeSnippets.push(new snippet(preSnippetBoundaryStringIndex + 1, text.substring(preSnippetBoundaryStringIndex + 4, postSnippetBoundaryStringIndex), postSnippetBoundaryStringIndex));
-            preSnippetBoundaryStringIndex = postSnippetBoundaryStringIndex + 1;
-        }else{
-            snippetSearchCompleted = true;
-        }
-    }while(snippetSearchCompleted == false);
+    textSnippets.forEach((arrayEntry) => {
+        (async() => {
+            if(arrayEntry.type == "typescript"){
+                if(arrayEntry.snippet.didOpen == false){
+                    connection.sendNotification("document/didOpen", JSON.stringify({textDocument: {uri: arrayEntry.snippet.uri, languageId: arrayEntry.snippet.languageId, version: arrayEntry.snippet.version, text: arrayEntry.snippet.text}}));
+                    myOutputs.appendLine(`\n-- SNIPPET SENT TO SERVER: (didOpen) --\ndocument:\n${JSON.stringify(arrayEntry.snippet)}\n-- End --`);
+                    arrayEntry.snippet.didOpen = true;
+                }else{
+                    arrayEntry.snippet.version += 1;
+                    connection.sendNotification("document/didChange", JSON.stringify({textDocument: {uri: arrayEntry.snippet.uri, languageId: arrayEntry.snippet.languageId, version: arrayEntry.snippet.version, text: arrayEntry.snippet.text}}));
+                    myOutputs.appendLine(`\n-- DOCUMENT UPDATE SENT TO SERVER: (didOpen) --\ndocument:\n${JSON.stringify(arrayEntry.snippet)}\n-- End --`);
+                }
+            }
+            let response = await connection.sendRequest("request", snippet.text);
+            myOutputs.appendLine(`\n-- SNIPPET SENT TO SERVER --\nRESPONSE:\n${JSON.stringify(response)}\n-- End --`);
+        })();
+    });
 
-    codeSnippets.forEach((snippet, index) => {myOutputs.appendLine(`\n-- Snippet ${index + 1} --\nStart index: ${snippet.start}\n-----\nText:\n-----\n${snippet.text}\n-----\nEnd index (exclusive): ${snippet.end}\n-----`);});
-    myOutputs.appendLine(`-- count: ${activationCount} --`);
-    activationCount++;
+    // codeSnippets.forEach((snippet, index) => {myOutputs.appendLine(`\n-- Snippet ${index + 1} --\nStart index: ${snippet.start}\n-----\nText:\n-----\n${snippet.text}\n-----\nEnd index (exclusive): ${snippet.end}\n-----`);});
+    // myOutputs.appendLine(`-- count: ${activationCount} --`);
+    // activationCount++;
 }
 
 
 async function handleRequests(){
+
     myOutputs = vscode.window.createOutputChannel("embeddedLsp output");
     myOutputs.show();
     var extensionTerminal = vscode.window.createTerminal("extension terminal");
@@ -148,12 +328,16 @@ async function handleRequests(){
     
     myOutputs.appendLine(`\n-- Server initialize response:\n"${JSON.stringify(serverInitializeResponse)}\n-- End of response --`);
 
+    connection.onNotification("publishDiagnostics", (diagnostics) => {
+        myOutputs.appendLine(`\n-- DIAGNOSTICS RECIEVED --\n${JSON.stringify(diagnostics)}\n-- End --`);
+    });
+
     doCompletions();
     //text = vscode.window.activeTextEditor.document.getText();
-    vscode.workspace.onDidChangeTextDocument(() => {
+    vscode.workspace.onDidChangeTextDocument((event) => {
         (async() => {
             newText = await getCurrentText();
-            if(newText != text){
+            if(event.document.uri == documentUri){ 
                 try{
                 doCompletions();
                 }catch(error){
@@ -165,12 +349,18 @@ async function handleRequests(){
     });
 }
 
+const targetNewDocumentCommand = "JsAndCssInCsharp.targetNewDocument";
 const startUpCommand = "JsAndCssInCsharp.startEmbeddedLsp";
+
+const targetNewTextDocument = () => {
+    documentUri = vscode.window.activeTextEditor.document.uri;
+}
 
 const runOnStartupCommand = () => {
     let run = true;
     if(run){
         vscode.window.showInformationMessage("embeddedLsp [1.0.7] is running");
+        targetNewTextDocument();
         handleRequests();
     }
     else
@@ -191,7 +381,7 @@ function activate(context)
     // """"
     
     
-
+    context.subscriptions.push(vscode.commands.registerCommand(targetNewDocumentCommand, targetNewTextDocument));
     
     context.subscriptions.push(vscode.commands.registerCommand(startUpCommand, runOnStartupCommand));
     
