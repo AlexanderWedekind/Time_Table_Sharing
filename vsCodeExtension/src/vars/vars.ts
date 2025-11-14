6a4b5b87343bef20edca6fe69ea76d9e255b668a
Alexander Wedekind <awedwork@gmail.com>
2025-11-14 22:05:53 +0000

    fixes type issues in vars.ts

--- vsCodeExtension/src/vars/vars.ts
+++ vsCodeExtension/src/vars/vars.ts
@@ -17,6 +17,117 @@ type Range = {
 
 type SnippetType = "typescript" | "cSharp";
 
+type Vars = typeof vars;
+
+//type Segment = typeof vars.textSnippet;
+
+//type SegmentListNode = null | TextSnippet;
+
+class TextSnippet {
+        type: SnippetType
+        range: Range
+        uri: string
+        didOpen: boolean
+        didChange: boolean
+        languageId: string
+        version: number
+        text: string
+        next: TextSnippet | null
+        previous: TextSnippet | null
+        sendDidOpen: Function
+        sendDidChange: Function
+        sendDidClose: Function
+        propertyEquals: Function
+
+        constructor(type: SnippetType, range: Range){
+            if(type == "typescript"){
+                this.type = type;
+                this.range = range;
+                this.uri = (() => {
+                    let count = 0;
+                    vars.documentSegmentList.forEachSegmentNode(
+                        (segment: TextSnippet) => {
+                            if(segment.type == "typescript"){
+                                count ++;
+                            }
+                        }
+                    );
+                    return `file:///${count}.ts`;
+                })();
+                this.didOpen = false;
+                this.didChange = false;
+                this.languageId = "typescript";
+                this.version = 1;
+                this.text = (() => {
+                    let text = "";
+                    let lineIndex = this.range.start.line;
+                    do{
+                        if(this.range.start.line == this.range.end.line){
+                            text = `${vars.currentText[lineIndex].substring(this.range.start.char, this.range.end.char)}`;
+                        }else if(lineIndex == this.range.start.line){
+                            text = `${text}${vars.currentText[lineIndex].substring(this.range.start.char)}`;
+                        }else if(lineIndex == this.range.end.line){
+                            text = `${text}${vars.currentText[lineIndex].substring(0, this.range.end.char)}`;
+                        }else if(lineIndex > this.range.start.line && lineIndex < this.range.end.line){
+                            text = `${text}${vars.currentText[lineIndex]}`;
+                        }
+                        lineIndex ++;
+                    }while(lineIndex != this.range.end.line + 1); 
+                    return text;
+                })();
+                this.next = null;
+                this.previous = null;
+                this.sendDidOpen = () => {
+                    this.didOpen = true;
+                    return {
+                        textDocument: {
+                            uri: this.uri,
+                            languageId: this.languageId,
+                            version: this.version,
+                            text: this.text
+                        }
+                    };
+                };
+                this.sendDidChange = () => {
+                    this.version += 1;
+                    return {
+                        textDocument: {
+                            uri: this.uri,
+                            version: this.version
+                        },
+                        contentChanges: [
+                            {
+                                text: this.text
+                            }
+                        ]
+                    }
+                };
+                this.sendDidClose = () => {
+                    return {
+                        textDocument: {
+                            uri: this.uri
+                        }
+                    };
+                };
+                this.propertyEquals = (value: any) => {
+                    let property = [this.type, this.range, this.uri, this.didOpen, this.didChange, this.languageId, this.version, this.text, this.next, this.previous];
+                    let propertyFound = false;
+                    property.forEach(
+                        (element) => {
+                            if(element == value){
+                                propertyFound = true;
+                            }
+                        }
+                    );
+                    return propertyFound;
+                }
+            }else if(type == "cSharp"){
+                this.type = type;
+                this.range = range;
+            }
+        }
+    }
+
 const vars = {
 
     currentDocUri: vscode.window.activeTextEditor?.document.uri as vscode.Uri | undefined,
@@ -72,7 +183,16 @@ const vars = {
     // },
     
     documentSegmentList: {
-        head: null,
+        head: null as null | TextSnippet,
+        addSegment: (newSegment: TextSnippet) => {
+            let lastSegment = vars.documentSegmentList.getLastSegment();
+            if(lastSegment != null){
+                lastSegment.next = newSegment;
+            }else{
+                vars.documentSegmentList.head = newSegment;
+            }
+            
+        },
         getSegments: (value: any) => {
             let segments = [];
             let currentSegmentNode = vars.documentSegmentList.head;
@@ -82,7 +202,10 @@ const vars = {
                     segments.push(currentSegmentNode);
                     }
                 }
-                currentSegmentNode = currentSegmentNode.next;
+                if(currentSegmentNode.next != null){
+                    currentSegmentNode = currentSegmentNode.next
+                }
+
             };
             
                 return segments;
@@ -106,112 +229,9 @@ const vars = {
         }
     },
 
-    textSnippet: class TextSnippet {
-        type: SnippetType
-        range: Range
-        uri: string
-        didOpen: boolean
-        didChange: boolean
-        languageId: string
-        version: number
-        text: string
-        next: TextSnippet | undefined
-        previous: TextSnippet | undefined
-        sendDidOpen: Function
-        sendDidChange: Function
-        sendDidClose: Function
-        propertyEquals: Function
+    textSnippet: TextSnippet, 
 
-        constructor(type: SnippetType, range: Range){
-            if(type == "typescript"){
-                this.type = type;
-                this.range = range;
-                this.uri = (() => {
-                    let count = 0;
-                    vars.documentSegmentList.forEachSegmentNode(
-                        (segment: TextSnippet) => {
-                            if(segment.type == "typescript"){
-                                count ++;
-                            }
-                        }
-                    );
-                    return `file:///${count}.ts`;
-                })();
-                this.didOpen = false;
-                this.didChange = false;
-                this.languageId = "typescript";
-                this.version = 1;
-                this.text = (() => {
-                    let text = "";
-                    let lineIndex = this.range.start.line;
-                    do{
-                        if(this.range.start.line == this.range.end.line){
-                            text = `${vars.currentText[lineIndex].substring(this.range.start.char, this.range.end.char)}`;
-                        }else if(lineIndex == this.range.start.line){
-                            text = `${text}${vars.currentText[lineIndex].substring(this.range.start.char)}`;
-                        }else if(lineIndex == this.range.end.line){
-                            text = `${text}${vars.currentText[lineIndex].substring(0, this.range.end.char)}`;
-                        }else if(lineIndex > this.range.start.line && lineIndex < this.range.end.line){
-                            text = `${text}${vars.currentText[lineIndex]}`;
-                        }
-                        lineIndex ++;
-                    }while(lineIndex != this.range.end.line + 1); 
-                    return text;
-                })();
-                this.next = undefined;
-                this.previous = undefined;
-                this.sendDidOpen = () => {
-                    this.didOpen = true;
-                    return {
-                        textDocument: {
-                            uri: this.uri,
-                            languageId: this.languageId,
-                            version: this.version,
-                            text: this.text
-                        }
-                    };
-                };
-                this.sendDidChange = () => {
-                    this.version += 1;
-                    return {
-                        textDocument: {
-                            uri: this.uri,
-                            version: this.version
-                        },
-                        contentChanges: [
-                            {
-                                text: this.text
-                            }
-                        ]
-                    }
-                };
-                this.sendDidClose = () => {
-                    return {
-                        textDocument: {
-                            uri: this.uri
-                        }
-                    };
-                };
-                this.propertyEquals = (value: any) => {
-                    let property = [this.type, this.range, this.uri, this.didOpen, this.didChange, this.languageId, this.version, this.text, this.next, this.previous];
-                    let propertyFound = false;
-                    property.forEach(
-                        (element) => {
-                            if(element == value){
-                                propertyFound = true;
-                            }
-                        }
-                    );
-                    return propertyFound;
-                }
-            }else if(type == "cSharp"){
-                this.type = type;
-                this.range = range;
-            }
-        }
-    }
-
-} as Vars;
+};
 
 
 
